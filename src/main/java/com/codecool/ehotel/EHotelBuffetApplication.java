cdf7b0b059c61965dd7c01e5ed4a3beb2c678c61
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-02-13 14:45:07 +0100

    ResourceManager got addGuestToList, EHotelBuffetApplication add guest as test.

--- src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
+++ src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
@@ -1,15 +1,27 @@
 package com.codecool.ehotel;
 
+import com.codecool.ehotel.logic.ResourceManager;
+import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.service.guest.GuestService;
+import com.codecool.ehotel.service.guest.GuestServiceImpl;
+
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
 public class EHotelBuffetApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         // Initialize services
-
+        GuestService guestService = new GuestServiceImpl();
         // Generate guests for the season
-
+        for (int i = 0; i < 100; i++){
+            ResourceManager.getInstance().addGuestToList(guestService.generateRandomGuest(LocalDate.of(2023, 10, 1), LocalDate.of(2023, 10, 10)));
+        }
         // Run breakfast buffet
-
+        for (Guest guest : ResourceManager.getInstance().getGuestList()) {
+            System.out.println(guest.name());
+        }
 
     }
 }
--- src/main/java/com/codecool/ehotel/logic/ResourceManager.java
+++ src/main/java/com/codecool/ehotel/logic/ResourceManager.java
@@ -1,16 +1,20 @@
 package com.codecool.ehotel.logic;
 
 import com.codecool.ehotel.model.Guest;
-
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResourceManager {
     private static final ResourceManager instance = new ResourceManager();
     private ResourceManager(){}
-    private static ResourceManager getInstance(){
+    public static ResourceManager getInstance(){
         return instance;
     };
-    List<Guest> guestList = new ArrayList<>();
+    private List<Guest> guestList = new ArrayList<>();
+    public List<Guest> getGuestList() {
+        return guestList;
+    }
+    public void addGuestToList(Guest guest){
+        guestList.add(guest);
+    }
 }
