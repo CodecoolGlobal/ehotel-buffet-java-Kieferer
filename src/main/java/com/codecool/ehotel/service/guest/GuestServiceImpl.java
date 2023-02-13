43264c9d2aadf8af7cdd69f8e90fb2a97b22dc85
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-02-13 13:01:50 +0100

    .gitignore not versioning .idea package anymore.

--- src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
+++ src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
@@ -1,7 +1,10 @@
 package com.codecool.ehotel.service.guest;
 
 import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.model.GuestType;
+import com.codecool.ehotel.utils.JSONReader;
 
+import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
@@ -9,7 +12,8 @@ import java.util.Set;
 public class GuestServiceImpl implements GuestService {
 
     @Override
-    public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) {
+    public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) throws FileNotFoundException {
+        //return new Guest(JSONReader.getRandomFromJSONFile(), GuestType.);
         return null;
     }
 
