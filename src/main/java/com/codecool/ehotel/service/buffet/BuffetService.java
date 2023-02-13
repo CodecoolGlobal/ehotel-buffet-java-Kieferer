e54548572b6df7f6f277da18a36d986d6bfa7066
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-02-13 12:36:02 +0100

    feat: Declaration and basic implementation done for consumeFreshest

--- src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
+++ src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
@@ -1,5 +1,9 @@
 package com.codecool.ehotel.service.buffet;
 
+import com.codecool.ehotel.model.Buffet;
+import com.codecool.ehotel.model.MealType;
+
 public interface BuffetService {
+    boolean consumeFreshest(Buffet currentBuffet, MealType meal);
 
 }
