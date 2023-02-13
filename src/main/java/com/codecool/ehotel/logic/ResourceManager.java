9d36021e8fefca55454febfda27cc40ffdaf6920
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-02-13 14:30:23 +0100

    ResourceManager become a singleton and store a guest list variable.

--- src/main/java/com/codecool/ehotel/logic/ResourceManager.java
+++ src/main/java/com/codecool/ehotel/logic/ResourceManager.java
@@ -1,4 +1,16 @@
 package com.codecool.ehotel.logic;
 
+import com.codecool.ehotel.model.Guest;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResourceManager {
+    private static final ResourceManager instance = new ResourceManager();
+    private ResourceManager(){}
+    private static ResourceManager getInstance(){
+        return instance;
+    };
+    List<Guest> guestList = new ArrayList<>();
 }
