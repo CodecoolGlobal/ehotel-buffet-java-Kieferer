893ac66d39aa04d5d3b02e17037a5b359919c602
Norbert Remenyi <remenyinorbert@gmail.com>
2023-02-14 14:06:41 +0100

    Add BreakfastManager class

--- src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
+++ src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
@@ -2,10 +2,9 @@ package com.codecool.ehotel;
 
 import com.codecool.ehotel.logic.ResourceManager;
 import com.codecool.ehotel.model.Buffet;
-import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.service.buffet.BuffetRefill;
 import com.codecool.ehotel.service.buffet.BuffetService;
 import com.codecool.ehotel.service.buffet.BuffetServiceImpl;
-import com.codecool.ehotel.service.buffet.buffetRefill;
 import com.codecool.ehotel.service.guest.GuestService;
 import com.codecool.ehotel.service.guest.GuestServiceImpl;
 
@@ -18,7 +17,7 @@ public class EHotelBuffetApplication {
         // Initialize services
         GuestService guestService = new GuestServiceImpl();
         BuffetService buffetService = new BuffetServiceImpl();
-        buffetRefill buffetManager = new buffetRefill();
+        BuffetRefill buffetManager = new BuffetRefill();
         ResourceManager.getInstance().setSimulationInterval(
                 LocalDate.of(2023, 10, 1),
                 LocalDate.of(2023, 10, 10));
