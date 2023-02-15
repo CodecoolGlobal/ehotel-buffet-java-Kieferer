3c1f0badb7757210bc148fe4b9523b86e49405c3
Norbert Remenyi <remenyinorbert@gmail.com>
2023-02-15 20:00:31 +0100

    Refill strategy merged into dinner-manager

--- src/main/java/com/codecool/ehotel/model/Ingredient.java
+++
@@ -1,6 +0,0 @@
-package com.codecool.ehotel.model;
-
-import java.util.List;
-
-public record Ingredient(IngredientType ingredientType, int amount) {
-}
--- src/main/java/com/codecool/ehotel/model/Kitchen.java
+++ src/main/java/com/codecool/ehotel/model/Kitchen.java
@@ -1,6 +1,23 @@
 package com.codecool.ehotel.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record Kitchen(List<Ingredient> ingredients, List<Ingredient>consumedIngredients) {
+public class Kitchen {
+    private final List<IngredientType> availableIngredients = new ArrayList<>();
+    private final List<IngredientType> consumedIngredients = new ArrayList<>();
+    public List<IngredientType> availableIngredients() { return availableIngredients; }
+    public List<IngredientType> consumedIngredients() { return consumedIngredients; }
+
+    public void addIngredients(IngredientType ingredient){
+        availableIngredients.add(ingredient);
+    }
+    public void addConsumedIngredients(IngredientType ingredient){
+        consumedIngredients.add(ingredient);
+    }
+    public void removeFromAvailableIngredients(IngredientType ingredient) { availableIngredients.remove(ingredient); }
+    public void removeAllFromAvailableIngredients(List<IngredientType> ingredientList){
+        this.availableIngredients.removeAll(ingredientList);
+    }
+
 }
