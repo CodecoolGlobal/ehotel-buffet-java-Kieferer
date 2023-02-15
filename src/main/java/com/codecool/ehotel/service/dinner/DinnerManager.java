f3bc4a9b5a795fa56f855beeebb888c0c048d38b
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-02-15 13:49:55 +0100

    feat: menu is done, with no preferences

--- src/main/java/com/codecool/ehotel/service/dinner/DinnerManager.java
+++ src/main/java/com/codecool/ehotel/service/dinner/DinnerManager.java
@@ -1,10 +1,20 @@
 package com.codecool.ehotel.service.dinner;
 
+import com.codecool.ehotel.model.MealType;
+
+import java.util.List;
+
 public class DinnerManager {
 
- /* TODO: -> Menu with statistics how much menu items are preferred by guests.
-             Individual preference list of the menu items generated upon the preference statistics.
+    /* TODO: -> Menu with statistics how much menu items are preferred by guests.
+                Individual preference list of the menu items generated upon the preference statistics.
 
+                //Done but with no preferences
+
+   */
+    List<MealType> menu = List.of(MealType.CEREAL, MealType.MILK, MealType.PANCAKE, MealType.FRIED_SAUSAGE, MealType.BUN, MealType.CROISSANT, MealType.FRIED_BACON, MealType.MASHED_POTATO, MealType.SCRAMBLED_EGGS);
+
+ /*
 
     TODO: -> Get the expected guests to appear(daily) as input-
             Guests have preference list - guests get happier if they can choose the top of their preferences from the menu
